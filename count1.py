6b5ef0fc049f1824e6396068c3d1e6c9bf2f9736
sundareshan1998 <46001087+Sundareshan98@users.noreply.github.com>
2019-08-04 21:43:12 +0530

    Rename count.py to count1.py

---
+++ count1.py
@@ -0,0 +1,13 @@
+a1=int(input())
+a=list(input().split())
+l=[]
+b=set(a)
+b=list(b)
+for i in range(0,len(b)):
+    b1=a.count(b[i])
+    if b1>1:
+        l.append(b[i])
+        
+    else:
+        continue
+print(*l)
